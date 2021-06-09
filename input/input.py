64b665d9e49f730dc8aebce3547854a3cef6a278
wonjun.sung <ruci.sung@gmail.com>
2021-06-09 15:31:22 +0900

    5

--- input/input.py
+++ input/input.py
@@ -1,14 +1,14 @@
-from __future__ import print_function
-import os, sys
-import pandas as pd
-import json
-import numpy as np
+import os
+from tensorflow.keras.datasets import mnist
 
-import tensorflow as tf
+os.chdir('..')
+os.makedirs('./data_in', exist_ok=True)
+current_dir = os.getcwd()
+(X_train, Y_train), (X_test, Y_test) = mnist.load_data(path=current_dir+'/data_in/mnist.npz')
 
-data_train, data_test = tf.keras.datasets.mnist.load_data()
-(images_train, labels_train) = data_train
-(images_test, labels_test) = data_test
+# data_train, data_test = tf.keras.datasets.mnist.load_data()
+# (images_train, labels_train) = data_train
+# (images_test, labels_test) = data_test
 
 # the data, split between train and test sets
 # os.makedirs('./data_in', exist_ok=True)
